3018f03a3e6acf3edf3fbc3a1023965e1937c205
vitidm <victordm97@gmail.com>
2023-04-18 23:47:22 +0200

    Add files via upload

--- getwhales.py
+++ getwhales.py
@@ -6,6 +6,11 @@ import pymysql
 
 UNISWAP_V2_SUBGRAPH_URL = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2"
 UNISWAP_V2_ROUTER_ADDRESS = "0x7a250d5630b4cf539739df2c5dacb4c659f2488d"
+UNISWAP_V3_SUBGRAPH_URL = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3"
+UNISWAP_V3_ROUTER_ADDRESS = "0xe592427a0aece92de3edee1f18e0157c05861564"
+SUSHISWAP_SUBGRAPH_URL = "https://api.thegraph.com/subgraphs/name/sushiswap/exchange"
+SUSHISWAP_ROUTER_ADDRESS = "0xd9e1ce17f2641f24ae83637ab66a2cca9c378b9f"
+
 WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
 def get_swaps_in_last_minutes(minutes):
     now_timestamp = int(time.time())
@@ -33,6 +38,66 @@ def get_swaps_in_last_minutes(minutes):
     else:
         raise Exception(f"Error fetching swaps data: {response.text}")
 
+def get_swaps_v3_in_last_minutes(minutes):
+    now_timestamp = int(time.time())
+    past_timestamp = now_timestamp - 60 * minutes
+
+    query = f"""
+    {{
+      swaps(first: 1000, where: {{timestamp_lte: "{now_timestamp}", origin: "{UNISWAP_V3_ROUTER_ADDRESS}", recipient_not_contains: "{UNISWAP_V3_ROUTER_ADDRESS}"}}, orderBy: timestamp, orderDirection: desc) {{
+        id
+        timestamp
+        origin
+        amount0
+        amount1
+        amountUSD
+        recipient
+        pool {{
+          token0 {{
+            symbol
+            name
+            id
+          }}
+          token1 {{
+            symbol
+            name
+            id
+          }}
+        }}
+      }}
+    }}
+    """
+
+    response = requests.post(UNISWAP_V3_SUBGRAPH_URL, json={'query': query})
+    if response.status_code == 200:
+        return json.loads(response.text)['data']['swaps']
+    else:
+        raise Exception(f"Error fetching swaps v3 data: {response.text}")
+
+def get_sushiswap_swaps_in_last_minutes(minutes):
+    now_timestamp = int(time.time())
+    past_timestamp = now_timestamp - 60 * minutes
+    
+    query = f"""
+    {{
+      swaps(first: 200, where: {{timestamp_lte: "{now_timestamp}"}}, orderBy: timestamp, orderDirection: desc) {{
+        id
+        timestamp
+        sender
+        amount0In
+        amount1In
+        amount0Out
+        amount1Out
+        to
+      }}
+    }}
+    """
+
+    response = requests.post(SUSHISWAP_SUBGRAPH_URL, json={'query': query})
+    if response.status_code == 200:
+        return json.loads(response.text)['data']['swaps']
+    else:
+        raise Exception(f"Error fetching SushiSwap swaps data: {response.text}")
 
 def parse_swap_id(swap_id):
     parts = swap_id.split("-")
@@ -72,6 +137,48 @@ def get_tx_hash_info(tx_hash):
 
     # Envía la consulta al subgrafo y obtiene la respuesta
     response = requests.post(UNISWAP_V2_SUBGRAPH_URL, json={'query': query})
+    # Si la respuesta es correcta, extrae los tokens del par
+    if response.status_code == 200:
+        data = json.loads(response.text)['data']
+        if data and 'transactions' in data and data['transactions']:
+            swaps = data['transactions'][0]['swaps']
+            if swaps:
+                token1 = swaps[0]['pair']['token1']
+                #print(f"El par de tokens del swap {tx_hash} es {token1['symbol']} | {token1['name']} | {token1['id']}")
+                token_symbol = token1['symbol']
+                token_name = token1['name']
+                token_address = token1['id']
+                return token_symbol, token_name, token_address
+        else:
+            print(f"No se encontró información para el swap con hash {tx_hash}")
+    else:
+        print(f"Error al obtener información del swap con hash {tx_hash}: {response.text}")
+
+def get_tx_hash_info_sushi(tx_hash):
+    # Define la consulta para obtener el par de tokens
+    query = f"""
+    {{
+    transactions(where: {{id: "{tx_hash}"}}) {{
+        swaps {{
+        pair {{
+            token0 {{
+            symbol
+            name
+            id
+            }}
+            token1 {{
+            symbol
+            name
+            id
+            }}
+        }}
+        }}
+    }}
+    }}
+    """
+
+    # Envía la consulta al subgrafo y obtiene la respuesta
+    response = requests.post(SUSHISWAP_SUBGRAPH_URL, json={'query': query})
 
     # Si la respuesta es correcta, extrae los tokens del par
     if response.status_code == 200:
@@ -108,8 +215,11 @@ def insert_sql_info(timestamp, wallet_sender, eth_in, tx_hash, token_name, token
 
 while True:
     print(f"Fetching swaps in the last {minutes} minutes...")
-    swaps = get_swaps_in_last_minutes(minutes)
-
+    swaps_v2 = get_swaps_in_last_minutes(minutes)
+    swaps_v3 = get_swaps_v3_in_last_minutes(minutes)
+    sushiswap_swaps = get_sushiswap_swaps_in_last_minutes(minutes)
+    swaps = swaps_v2 + swaps_v3
+    print("-------------- UNISWAP --------------")
     for swap in swaps:
         swap_id = swap['id']
 
@@ -129,7 +239,38 @@ while True:
                     wallet_sender = swap['to']
                     eth_in = float(swap['amount0In'])
                     tokens_out = swap['amount0Out']
-                    if eth_in >= 1.0 and eth_in <= 20.0:
+                    if eth_in >= 0.8 and eth_in <= 20.0:
+                        print(swap)
+                        print(f"Swap ID: {swap_id}")
+                        print(f"  Tx Hash: {tx_hash}")
+                        print(f"  TOKEN INFO: \n  - Token Symbol: {token_symbol}\n  - Token Name: {token_name}\n  - Token Address: {token_address}")
+                        print(f"  Timestamp: {timestamp}")
+                        print(f"  Wallet: {wallet_sender}")
+                        print(f"  Amount ETH In: {eth_in}")
+                        print(f"  Amount Tokens Out: {tokens_out}")
+                        print()
+                        insert_sql_info(timestamp, wallet_sender, eth_in, tx_hash, token_name, token_symbol, token_address)
+            except Exception:
+                continue
+    print("################# SUSHISWAP MARK ###################")
+    for swap in sushiswap_swaps:
+        swap_id = swap['id']
+
+        if swap_id not in processed_swaps:
+            processed_swaps.add(swap_id)
+            swap_info = parse_swap_id(swap['id'])
+            # Para los swaps de SushiSwap, la dirección del par ya está en el campo 'id'
+            
+            try:
+                token_symbol, token_name, token_address = get_tx_hash_info_sushi(swap_info['pair_address'])
+                if token_symbol != "WETH" and token_symbol != "USDT" and token_symbol != "UNI-V2" and token_symbol != "GETH" and token_symbol != "USDC":
+                    tx_hash = swap_info['pair_address']
+                    timestamp = datetime.datetime.fromtimestamp(int(swap['timestamp']))
+                    timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+                    wallet_sender = swap['to']
+                    eth_in = float(swap['amount0In'])
+                    tokens_out = swap['amount0Out']
+                    if eth_in >= 0.8 and eth_in <= 20.0:
                         print(swap)
                         print(f"Swap ID: {swap_id}")
                         print(f"  Tx Hash: {tx_hash}")
